f43434f8252e01a3f27cc4ebc97020af81c2f134
JuanCohetes <150453155+JuanCohetes@users.noreply.github.com>
2023-11-10 09:56:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ import streamlit as st
 
 st.title("Mi primera app")
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('medellin.jpg')
