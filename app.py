7b335653ae3f60fb7ec52bda914bf1ed57208932
JuanCohetes <150453155+JuanCohetes@users.noreply.github.com>
2023-11-10 10:03:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ image = Image.open('medellin.jpg')
 
 st.image(image, caption='Calles de Medellin')
 
-texto = st.text_input('Escribe algo', 'Este es mi texto')
+texto = st.text_input('Escribe algo', 'Nombre Barrio')
 st.write('El texto escrito es', texto)
 
 st.subheader("Ahora usemos 2 Columnas")
@@ -24,7 +24,7 @@ with col1:
   
 with col2:
     st.subheader("Esta es la segunda columna")
-    modo = st.radio("Que Modalidad es la principal en tu interfaz", ('Visual', 'auditiva', 'Táctil'))
+    modo = st.radio("Estrato socioeconomico", ('1', '2, '3','4','5'))
     if modo == 'Visual':
        st.write('La vista es fundamental para tu interfaz')
     if modo == 'auditiva':
